4f5ee9e01124d6ecdb87e1651074cb15122c6ede
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-12-16 02:10:50 +0800

    mydan/uninstall.sh先卸载mayi，后卸载perl

--- scripts/mydan/uninstall.sh
+++ scripts/mydan/uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/MYDan/perl/master/scripts/uninstall.sh |bash
 curl -s https://raw.githubusercontent.com/MYDan/openapi/master/scripts/mayi/uninstall.sh |bash
+curl -s https://raw.githubusercontent.com/MYDan/perl/master/scripts/uninstall.sh |bash
