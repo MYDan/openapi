a5f4e0fee6b2be55ae6b9ae0914f4ef1589a9620
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-12-16 03:03:33 +0800

    mydan 添加mydan/.lock判断

--- scripts/mydan/install.sh
+++ scripts/mydan/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ -f /opt/mydan/.lock ]; then
+    echo "The mydan is locked"
+    exit;
+fi
+
 curl -s https://raw.githubusercontent.com/MYDan/perl/master/scripts/install.sh |bash
 curl -s https://raw.githubusercontent.com/MYDan/openapi/master/scripts/mayi/install.sh |bash
--- scripts/mydan/update.sh
+++ scripts/mydan/update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ -f /opt/mydan/.lock ]; then
+    echo "The mydan is locked"
+    exit;
+fi
+
 curl -s https://raw.githubusercontent.com/MYDan/perl/master/scripts/update.sh |bash
 curl -s https://raw.githubusercontent.com/MYDan/openapi/master/scripts/mayi/update.sh |bash
