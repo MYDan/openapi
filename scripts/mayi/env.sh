75074a6f5caf6de19e8b7ad01e57449835d0ec11
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-12-21 18:06:27 +0800

    update mayi/env.sh

--- scripts/mayi/env.sh
+++ scripts/mayi/env.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
-BP='/opt/mydan'
+INSTALLERDIR='/opt/mydan'
 
-if [ -f $BP/dan/.lock ]; then
+if [ -f $INSTALLERDIR/dan/.lock ]; then
     echo "The mayi is locked"
     exit;
 fi
 
-mkdir -p $BP/etc
+mkdir -p $INSTALLERDIR/etc
 
-> $BP/etc/env.tmp
+> $INSTALLERDIR/etc/env.tmp
 if [ -n "$ORGANIZATION" ];then
-    echo "ORGANIZATION=$ORGANIZATION" >> $BP/etc/env.tmp
+    echo "ORGANIZATION=$ORGANIZATION" >> $INSTALLERDIR/etc/env.tmp
 fi
 
 if [ -n "$MYDAN_KEY_UPDATE" ];then
-    echo "MYDAN_KEY_UPDATE=$MYDAN_KEY_UPDATE" >> $BP/etc/env.tmp
+    echo "MYDAN_KEY_UPDATE=$MYDAN_KEY_UPDATE" >> $INSTALLERDIR/etc/env.tmp
 fi
 
 if [ -n "$MYDAN_PROC_UPDATE" ];then
-    echo "MYDAN_PROC_UPDATE=$MYDAN_PROC_UPDATE" >> $BP/etc/env.tmp
+    echo "MYDAN_PROC_UPDATE=$MYDAN_PROC_UPDATE" >> $INSTALLERDIR/etc/env.tmp
 fi
 if [ -n "$MYDAN_WHITELIST_UPDATE" ];then
-    echo "MYDAN_WHITELIST_UPDATE=$MYDAN_WHITELIST_UPDATE" >> $BP/etc/env.tmp
+    echo "MYDAN_WHITELIST_UPDATE=$MYDAN_WHITELIST_UPDATE" >> $INSTALLERDIR/etc/env.tmp
 fi
 
 if [ -n "$MYDAN_UPDATE" ];then
-    echo "MYDAN_UPDATE=$MYDAN_UPDATE" >> $BP/etc/env.tmp
+    echo "MYDAN_UPDATE=$MYDAN_UPDATE" >> $INSTALLERDIR/etc/env.tmp
 fi
 
-if [[ -s $BP/etc/env.tmp ]];then
-    mv $BP/etc/env.tmp $BP/etc/env
+if [[ -s $INSTALLERDIR/etc/env.tmp ]];then
+    mv $INSTALLERDIR/etc/env.tmp $INSTALLERDIR/etc/env
 else
-    rm $BP/etc/env
-    rm $BP/etc/env.tmp
+    rm $INSTALLERDIR/etc/env
+    rm $INSTALLERDIR/etc/env.tmp
 fi
 
 
-if [ -f $BP/etc/env ];then
-    $BP/dan/bootstrap/bin/bootstrap --install
+if [ -f $INSTALLERDIR/etc/env ];then
+    $INSTALLERDIR/dan/bootstrap/bin/bootstrap --install
 else
-    $BP/dan/bootstrap/bin/bootstrap --uninstall
+    $INSTALLERDIR/dan/bootstrap/bin/bootstrap --uninstall
 fi
 
 echo OK
