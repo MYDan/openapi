eb5d89b802cd86a74a9b98ebdc10ebb461f94d77
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-12-21 18:15:30 +0800

    update mydan/*

--- scripts/mydan/update.sh
+++ scripts/mydan/update.sh
@@ -5,5 +5,35 @@ if [ -f /opt/mydan/.lock ]; then
     exit;
 fi
 
-curl -s https://raw.githubusercontent.com/MYDan/perl/master/scripts/update.sh |bash
+OS=$(uname)
+ARCH=$(uname -m)
+
+for T in "Linux:x86_64" "Darwin:x86_64"
+do
+    o=$(echo $T|awk -F: '{print $1}')
+    a=$(echo $T|awk -F: '{print $2}')
+    [ "X$OS" == "X$o" ] && [ "X$ARCH" == "X$a" ]&&  break
+done
+
+if [ "X$OS" == "X$o" ] && [ "X$ARCH" == "X$a" ]; then
+    echo "OS:$OS ARCH:$ARCH ok"
+else
+    echo "OS:$OS ARCH:$ARCH Not supported"
+    exit 1
+fi
+
+checktool() {
+    if ! type $1 >/dev/null 2>&1; then
+        echo "Need tool: $1"
+        exit 1;
+    fi
+}
+
+if [ "X$OS" == "XDarwin" ]; then
+    checktool cpan   
+    cpan install MYDan
+else
+    curl -s https://raw.githubusercontent.com/MYDan/perl/master/scripts/update.sh |bash
+fi
+
 curl -s https://raw.githubusercontent.com/MYDan/openapi/master/scripts/mayi/update.sh |bash
