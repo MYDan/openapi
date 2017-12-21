eb92e68298bbe44e5336a7d5c36fb7320f283059
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-12-21 18:31:40 +0800

    update mydan/*

--- scripts/mydan/install.sh
+++ scripts/mydan/install.sh
@@ -8,20 +8,6 @@ fi
 OS=$(uname)
 ARCH=$(uname -m)
 
-for T in "Linux:x86_64" "Darwin:x86_64"
-do
-    o=$(echo $T|awk -F: '{print $1}')
-    a=$(echo $T|awk -F: '{print $2}')
-    [ "X$OS" == "X$o" ] && [ "X$ARCH" == "X$a" ]&&  break
-done
-
-if [ "X$OS" == "X$o" ] && [ "X$ARCH" == "X$a" ]; then
-    echo "OS:$OS ARCH:$ARCH ok"
-else
-    echo "OS:$OS ARCH:$ARCH Not supported"
-    exit 1
-fi
-
 checktool() {
     if ! type $1 >/dev/null 2>&1; then
         echo "Need tool: $1"
@@ -29,11 +15,18 @@ checktool() {
     fi
 }
 
-if [ "X$OS" == "XDarwin" ]; then
+for T in "Linux:x86_64"
+do
+    o=$(echo $T|awk -F: '{print $1}')
+    a=$(echo $T|awk -F: '{print $2}')
+    [ "X$OS" == "X$o" ] && [ "X$ARCH" == "X$a" ]&&  break
+done
+
+if [ "X$OS" == "X$o" ] && [ "X$ARCH" == "X$a" ]; then
+    curl -s https://raw.githubusercontent.com/MYDan/perl/master/scripts/install.sh |bash
+else
     checktool cpan   
     cpan install MYDan
-else
-    curl -s https://raw.githubusercontent.com/MYDan/perl/master/scripts/install.sh |bash
 fi
 
 curl -s https://raw.githubusercontent.com/MYDan/openapi/master/scripts/mayi/install.sh |bash
--- scripts/mydan/uninstall.sh
+++ scripts/mydan/uninstall.sh
@@ -8,7 +8,7 @@ fi
 OS=$(uname)
 ARCH=$(uname -m)
 
-for T in "Linux:x86_64" "Darwin:x86_64"
+for T in "Linux:x86_64" 
 do
     o=$(echo $T|awk -F: '{print $1}')
     a=$(echo $T|awk -F: '{print $2}')
@@ -16,16 +16,6 @@ do
 done
 
 if [ "X$OS" == "X$o" ] && [ "X$ARCH" == "X$a" ]; then
-    echo "OS:$OS ARCH:$ARCH ok"
-else
-    echo "OS:$OS ARCH:$ARCH Not supported"
-    exit 1
-fi
-
-
-if [ "X$OS" == "XDarwin" ]; then
-    echo "$OS perl not need to uninstall"
-else
     curl -s https://raw.githubusercontent.com/MYDan/perl/master/scripts/uninstall.sh |bash
 fi
 
