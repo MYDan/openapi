24ebe94e70fb7cf7ea9844cb5c978bdab7aca881
lijinfeng2011 <lijinfeng2011@gmail.com>
2018-09-30 22:46:57 +0800

    mydan/update.sh 添加mac上安装的脚本

--- scripts/mydan/update.sh
+++ scripts/mydan/update.sh
@@ -8,7 +8,53 @@ fi
 #export MYDAN_REPO_PUBLIC="http://180.153.186.60 http://223.166.174.60"
 #MYDAN_REPO_PRIVATE
 
-curl -k -s https://raw.githubusercontent.com/MYDan/perl/master/scripts/update.sh |bash || exit 1
-curl -k -s https://raw.githubusercontent.com/MYDan/openapi/master/scripts/mayi/update.sh |bash || exit 1
+OS=$(uname)
+ARCH=$(uname -m)
+
+if [ "X$OS" == "XDarwin" ] && [ "X$ARCH" == "Xx86_64" ]; then
+    echo OS=$OS
+
+    set -e
+    mkdir -p /opt/mydan 
+    cd /opt/mydan
+
+    set +e
+    cpan install Types::Standard
+
+    perl -e 'use Types::Standard' 2>/dev/null
+
+    if [ "X$?" != "X0" ]; then
+        set -e
+        rm -rf Type-Tiny-1.004002.tar.gz Type-Tiny-1.004002
+
+        wget https://cpan.metacpan.org/authors/id/T/TO/TOBYINK/Type-Tiny-1.004002.tar.gz
+        tar -zxvf Type-Tiny-1.004002.tar.gz
+        cd Type-Tiny-1.004002
+        perl Makefile.PL
+        make
+        make install
+
+        rm -rf Type-Tiny-1.004002.tar.gz Type-Tiny-1.004002
+    fi
+
+    set -e
+    cd /opt/mydan
+    rm -rf mayi
+    git clone https://github.com/MYDan/mayi.git
+    cd mayi
+
+    cat Makefile.PL|grep ::|grep '=> \d'|awk '{print $1}'|xargs -I{} cpan install {} || echo fail
+
+    perl Makefile.PL
+    make
+    make install dan=1 box=1 def=1
+    rm -rf /opt/mydan/mayi
+
+else
+
+    curl -k -s https://raw.githubusercontent.com/MYDan/perl/master/scripts/update.sh |bash || exit 1
+    curl -k -s https://raw.githubusercontent.com/MYDan/openapi/master/scripts/mayi/update.sh |bash || exit 1
+
+fi
 
 echo mydan update OK
